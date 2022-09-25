59704e76b6e634a8d54cf9feb9c3cdc62d655a59
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-09-26 01:25:19 +0300

    DOM Manipulation
    Some simple functions about my DOM manipulation lection from softuni Advance module

--- SoftUni Advanced/Lection11-DOM-Manipulation and Events/01. List-Of-Items/app.js
+++ SoftUni Advanced/Lection11-DOM-Manipulation and Events/01. List-Of-Items/app.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+    let ulItems = document.getElementById('items');
+    let newItem = document.getElementById('newItemText');
+    
+    let liElement = document.createElement('li');
+    liElement.textContent = newItem.value;
+
+    ulItems.appendChild(liElement);
+
+    
 }
--- SoftUni Advanced/Lection11-DOM-Manipulation and Events/02. Delete-From-Table/app.js
+++ SoftUni Advanced/Lection11-DOM-Manipulation and Events/02. Delete-From-Table/app.js
@@ -1,3 +1,24 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let emailInput = document.querySelector('input[name = "email"]');
+    let resultEl = document.getElementById('result')
+    let custumersEmails = document.querySelectorAll('tr td:nth-of-type(2)');
+
+    let custumersArr = Array.from(custumersEmails)
+    let targetElement = custumersArr.find(x => x.textContent == emailInput.value);
+    
+    if (targetElement) {
+        targetElement.parentNode.remove();
+        resultEl.textContent = 'Deleted.'
+    }else{
+        resultEl.textContent = 'Not found';
+    }
 }
+
+    //Old Way of deleting  child elements
+    //  targetElement.parentNode.removeChild(targetElement);
+
+
+    //New and easyer way of deleting childNodes
+    // targetElement.remove()
+
+    //Deleting the whole parrent row
