d15ff4993be9dc896bf6d4fcd879142c74672b67
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-09-26 03:55:11 +0300

    DOM Manipulation
    DOM Manipulation

--- SoftUni Advanced/Lection11-DOM-Manipulation and Events/04. Mouse-Gradient/app.js
+++ SoftUni Advanced/Lection11-DOM-Manipulation and Events/04. Mouse-Gradient/app.js
@@ -1,3 +1,28 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradient = document.getElementById('gradient');
+    let result = document.getElementById('result');
+
+    gradient.addEventListener('mousemove', gradientMove);
+    gradient.addEventListener('mouseout', gradientOut);
+
+    function gradientMove(event) {
+        let power = event.offsetX / (event.target.clientWidth - 1);
+        power = Math.trunc(power * 100);
+
+        result.textContent = power + "%"
+    }
+    function gradientOut(event) {
+        result.textContent = "";
+    }
+    
 }
+
+
+// const gradientMousemoveHandler = (e) => {
+//     let percent = (e.offsetX / e.target.offsetWidth) *100
+//    percent= percent.toFixed(0);
+   
+//     result.textContent = `${percent}%`
+// }
+
+// gradient.addEventListener('mousemove', gradientMousemoveHandler)
