3e9accffdd0a20fb09b2df8a7fcc754caa0b7172
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-09-28 02:27:48 +0300

    DOM Events
    Some DOM events from my JS advance softuni course

--- SoftUni Advanced/lection12_DOM_Events_Exercises/03. Locked-Profile/app.js
+++ SoftUni Advanced/lection12_DOM_Events_Exercises/03. Locked-Profile/app.js
@@ -1,3 +1,21 @@
 function lockedProfile() {
-    console.log('TODO...')
+  Array.from(document.querySelectorAll(".profile button")).forEach((button) =>
+    button.addEventListener("click", onClick)
+  );
+
+  function onClick(ev) {
+    let profile = ev.target.parentElement;
+    let isUnlocked = profile.querySelectorAll("input[type='radio']")[1].checked;
+    let hidden = profile.querySelector("div");
+
+    if (isUnlocked) {
+      if (ev.target.textContent === "Show more") {
+        hidden.style.display = "block";
+        ev.target.textContent = "Hide it";
+      } else if (ev.target.textContent === "Hide it") {
+        hidden.style.display = "none";
+        ev.target.textContent = "Show more";
+      }
+    }
+  }
 }
