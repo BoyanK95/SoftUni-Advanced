26a726c4367e85965ba71cedb6172f1e5384f341
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-09-28 15:22:18 +0300

    DOM Events
    DOM Events from my JS advance module

--- SoftUni Advanced/lection12_DOM_Events_Exercises/04. Fill-Dropdown/dropdown.js
+++ SoftUni Advanced/lection12_DOM_Events_Exercises/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+  let dropDownMenu = document.getElementById("menu");
+  let inputText = document.querySelectorAll("input")[0];
+  let inputValue = document.querySelectorAll("input")[1];
+
+  let optionEl = document.createElement("option");
+  optionEl.textContent = inputText.value;
+  optionEl.value = inputValue.value;
+
+  dropDownMenu.appendChild(optionEl);
+  inputText.value = "";
+  inputValue.value = "";
 }
