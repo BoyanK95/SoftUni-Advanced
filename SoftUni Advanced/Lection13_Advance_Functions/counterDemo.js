19b841eacf230c4d9ae59612801c539ac94269e8
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-09-28 20:20:21 +0300

    Advance Functions
    Advance Functions from my softuni advance module

---
+++ SoftUni Advanced/Lection13_Advance_Functions/counterDemo.js
@@ -0,0 +1,15 @@
+function counterBuilder() {
+    let counter = 0;
+
+    return function () {
+        counter++;
+        console.log(counter);
+    }
+}
+
+let counter = counterBuilder();
+counter();
+counter();
+counter();
+counter();
+counter();
