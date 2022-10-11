8f063dc7b891fd56d5e964affb8a9f9417a0bc9d
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-10-11 22:03:21 +0300

    Classes
    Classes  Prototypes and inheritance from my softuni js advance modle

---
+++ SoftUni Advanced/Lection20Proto-Inher-Exercises/demo.js
@@ -0,0 +1,15 @@
+function solve() {
+    class Cat{
+        constructor(name){
+            this.name = name
+        }
+
+        get name(){
+            return this._name
+        }
+
+        set name(value){
+            this._name = value
+        }
+    }
+}
