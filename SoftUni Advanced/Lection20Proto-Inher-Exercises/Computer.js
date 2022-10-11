a8be79dfbee8066c6fc095065a97eddda38a42af
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-10-11 22:14:42 +0300

    Added game-screen

---
+++ SoftUni Advanced/Lection20Proto-Inher-Exercises/Computer.js
@@ -0,0 +1,16 @@
+function createComputerHierarchy() {
+    
+
+
+    
+}
+
+
+
+let classes = createComputerHierarchy();
+let Computer = classes.Computer;
+let Laptop = classes.Laptop;
+let Desktop = classes.Desktop;
+let Monitor = classes.Monitor;
+let Battery = classes.Battery;
+let Keyboard = classes.Keyboard;
