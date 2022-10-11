de8f40c4f5962682ca2e67ba9aa123ecb1f642cd
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-10-12 01:25:21 +0300

    Added wizard movement

--- SoftUni Advanced/Wizard-Game/src/js/gameEngine.js
+++ SoftUni Advanced/Wizard-Game/src/js/gameEngine.js
@@ -12,13 +12,23 @@ function gameLoop(state, game) {
 
 
     // Move wizard
-    if (state.keys.KeyD) {
-        console.log(state.keys);
-       wizard.posX += 10;
+    if (state.keys.KeyA) {
+       wizard.posX -= wizard.speed;
     }
+
+    if (state.keys.KeyS) {
+        wizard.posY += wizard.speed;
+     }
+    if (state.keys.KeyD) {
+        wizard.posX += wizard.speed;
+    }
+    if (state.keys.KeyW) {
+        wizard.posY -= wizard.speed;
+     }
 
     //Render
     wizardElement.style.left = wizard.posX + 'px';
+    wizardElement.style.top = wizard.posY + 'px';
 
     window.requestAnimationFrame(gameLoop.bind(null, state, game))
 
--- SoftUni Advanced/Wizard-Game/src/js/gameState.js
+++ SoftUni Advanced/Wizard-Game/src/js/gameState.js
@@ -7,9 +7,9 @@ function initState() {
         wizard: {
             width: 82,
             height: 100,
-
             posX: startX,
             posY: startY,
+            speed: 10,
         },
         keys: {}
     }
