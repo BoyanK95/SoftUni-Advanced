b1a9e5fa331bbb3e54197f221ec689c9b41dc9c8
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-10-12 01:53:28 +0300

    Added wizard movement boundaries

--- SoftUni Advanced/Wizard-Game/src/js/gameEngine.js
+++ SoftUni Advanced/Wizard-Game/src/js/gameEngine.js
@@ -10,20 +10,20 @@ function gameLoop(state, game) {
     const { wizard } = state;
     const {wizardElement } = game
 
-
     // Move wizard
     if (state.keys.KeyA) {
-       wizard.posX -= wizard.speed;
+       wizard.posX = Math.max(wizard.posX - wizard.speed, 0);
     }
 
     if (state.keys.KeyS) {
-        wizard.posY += wizard.speed;
+        wizard.posY = Math.min(wizard.posY + wizard.speed, game.gameScreen.offsetHeight - wizard.height);
      }
     if (state.keys.KeyD) {
-        wizard.posX += wizard.speed;
+        wizard.posX = Math.min(wizard.posX + wizard.speed, game.gameScreen.offsetWidth - wizard.width);
+        
     }
-    if (state.keys.KeyW) {
-        wizard.posY -= wizard.speed;
+    if (state.keys.KeyW && wizard.posY > 0) {
+        wizard.posY = Math.max(wizard.posY - wizard.speed, 0);
      }
 
     //Render
