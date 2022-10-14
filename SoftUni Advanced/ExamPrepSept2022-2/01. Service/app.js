dd4209ce0cc703236256b2d46bc4ba68b88776bb
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-10-14 22:16:01 +0300

    Exam Prep September 2022
    Exam Prep September 2022  Second lecture

---
+++ SoftUni Advanced/ExamPrepSept2022-2/01. Service/app.js
@@ -0,0 +1,57 @@
+window.addEventListener("load", solve);
+
+function solve() {
+    document.querySelector('button[type="submit"]').addEventListener('click', createTask)
+    let productTypeInput = document.getElementById('type-product');
+    let descriptionInput = document.getElementById('description')
+    let nameInput = document.getElementById('client-name');
+    let phoneNumInput = document.getElementById('client-phone');
+    let recivedOrdersSection = document.getElementById('received-orders')
+
+
+    function createTask(e) {
+        e.preventDefault()
+        let productType = productTypeInput.value
+        let description = descriptionInput.value
+        let clienName = nameInput.value
+        let phoneNum = phoneNumInput.value
+
+        if (!description || !clienName || !phoneNum) {
+            return
+        }
+
+        productType.value = '';
+        descriptionInput.value = ''
+        nameInput.value = ''
+        phoneNumInput.value = ''
+
+        createOrder(productType, description, clienName, phoneNum)
+    }
+    function createOrder(productType, description, clienName, phoneNum) {
+        let divContainer = document.createElement('div');
+        divContainer.classList.add('container');
+
+        let h2El = document.createElement('h2');
+        h2El.textContent = `Product type for repair: ${productType}`
+        let h3El = document.createElement('h3');
+        h3El.textContent = `Client information: ${clienName}, ${phoneNum}`
+        let h4El = document.createElement('h4');
+        h4El.textContent = `Description of the problem: ${description}`
+
+        let startRepairBtn = document.createElement('button')
+        startRepairBtn.classList.add('start-btn')
+        startRepairBtn.textContent = 'Start repair'
+
+        let finishBtn = document.createElement('button');
+        finishBtn.classList.add('finish-btn');
+        finishBtn.textContent = 'Finish repair';
+        finishBtn.setAttribute('disabled', true);
+
+        divContainer.appendChild(h2El);
+        divContainer.appendChild(h3El);
+        divContainer.appendChild(h4El);
+        divContainer.appendChild(startRepairBtn);
+        divContainer.appendChild(finishBtn);
+        recivedOrdersSection.appendChild(divContainer);
+    }
+}
