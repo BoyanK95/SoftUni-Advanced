813ce6f4bde2069a8918910bbd37aa8bcf9d1073
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-10-16 01:56:04 +0300

    Exam prep DOM Manipulation
    Made the sellCar btn and the editCar Btn functional

--- SoftUni Advanced/Exam25Jun2022/Problem1 CatDealers/app.js
+++ SoftUni Advanced/Exam25Jun2022/Problem1 CatDealers/app.js
@@ -8,6 +8,10 @@ function solve() {
   const costInput = document.getElementById('original-cost');
   const sellPriceInput = document.getElementById('selling-price');
   const tableBodySection = document.getElementById('table-body');
+  const soldCarsSection = document.getElementById('cars-list');
+  let totalProfitMadeInput = document.getElementById('profit')
+
+  
 
   const publishBtn = document.getElementById('publish');
   publishBtn.addEventListener('click', publishCar);
@@ -49,10 +53,12 @@ function solve() {
     let editBtn = document.createElement('button')
     editBtn.className = "action-btn edit";
     editBtn.textContent = `Edit`;
+    editBtn.addEventListener('click', editCar)
 
     let sellBtn = document.createElement('button');
     sellBtn.className = "action-btn sell";
     sellBtn.textContent = 'Sell'
+    sellBtn.addEventListener('click', sellCar)
 
     btnTdContainer.appendChild(editBtn)
     btnTdContainer.appendChild(sellBtn)
@@ -73,5 +79,51 @@ function solve() {
     tableRowEl.appendChild(sellPriceTdEl)
     tableRowEl.appendChild(btnTdContainer)
     tableBodySection.appendChild(tableRowEl)
+  }
+  function editCar(e) {
+    let dataEl = e.currentTarget.parentElement.parentElement
+    makeInput.value = dataEl.querySelectorAll('td')[0].textContent
+    modelInput.value = dataEl.querySelectorAll('td')[1].textContent
+    yearInput.value = dataEl.querySelectorAll('td')[2].textContent
+    fuelTypeInput.value = dataEl.querySelectorAll('td')[3].textContent
+    costInput.value = dataEl.querySelectorAll('td')[4].textContent
+    sellPriceInput.value = dataEl.querySelectorAll('td')[5].textContent
+
+    dataEl.remove()
+  }
+
+  function sellCar(e) {
+    let dataEl = e.currentTarget.parentElement.parentElement;
+    let totalProfit = Number(totalProfitMadeInput.innerText)
+    let make = dataEl.querySelectorAll('td')[0].textContent;
+    let model = dataEl.querySelectorAll('td')[1].textContent;
+    let year = dataEl.querySelectorAll('td')[2].textContent
+    let costPrice = Number(dataEl.querySelectorAll('td')[4].textContent);
+    let sellPrice = Number(dataEl.querySelectorAll('td')[5].textContent);
+    let profitFromCar = sellPrice - costPrice
+
+    totalProfit += profitFromCar;
+    Math.round(totalProfit).toFixed(2)
+
+    let rootLiEl = document.createElement('li')
+    rootLiEl.classList.add('each-list');
+
+    let span1 = document.createElement('span')
+    let span2 = document.createElement('span')
+    let span3 = document.createElement('span')
+
+    span1.textContent = `${make} ${model}`;
+    span2.textContent = `${year}`;
+    span3.textContent = profitFromCar;
+
+    rootLiEl.appendChild(span1)
+    rootLiEl.appendChild(span2)
+    rootLiEl.appendChild(span3)
+    soldCarsSection.appendChild(rootLiEl);
+
+    dataEl.remove();
+
+    totalProfitMadeInput.textContent = totalProfit.toFixed(2)
+    
   }
 }
