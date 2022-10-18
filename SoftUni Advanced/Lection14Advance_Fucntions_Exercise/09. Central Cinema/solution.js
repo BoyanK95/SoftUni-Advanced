343e931b14689bb2525f73f25e59bee78f8153b3
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-10-19 02:12:01 +0300

    finished Movie Hall functionality

--- SoftUni Advanced/Lection14Advance_Fucntions_Exercise/09. Central Cinema/solution.js
+++ SoftUni Advanced/Lection14Advance_Fucntions_Exercise/09. Central Cinema/solution.js
@@ -2,6 +2,24 @@ function solve() {
     const containerElement = document.getElementById('container')
     let inputFields = containerElement.querySelectorAll('input')
     const movieUlSection = document.getElementById('movies').querySelector('ul')
+    const archiveSection = document.getElementById('archive')
+    const ulArchiveRoot = archiveSection.querySelector('ul')
+    let archiveSectionBtns = Array.from(archiveSection.querySelectorAll('button'))
+
+    for (let button of archiveSectionBtns) {
+        if (button.textContent == 'Clear') {
+            button.addEventListener('click', clear)
+        }
+    }
+    function clear(e) {
+        let liElements = Array.from(e.currentTarget.parentElement.querySelectorAll('li'))
+        liElements.forEach(element => {
+            element.remove()
+        });
+        
+    }
+
+    
     
     const onScreenBtn = document.querySelector('button')
     onScreenBtn.addEventListener('click', createMovie)
@@ -37,6 +55,8 @@ function solve() {
         inputEl.setAttribute('placeholder', 'Tickets Sold')
         let archiveBtn = document.createElement('button')
         archiveBtn.textContent = 'Archive'
+        
+        archiveBtn.addEventListener('click', archive)
 
         spanNameEl.textContent = nameInput
         strongHallEl.textContent = `Hall: ${hall}`
@@ -51,5 +71,35 @@ function solve() {
         divEl.appendChild(archiveBtn)
         liRootEl.appendChild(divEl)
         movieUlSection.appendChild(liRootEl)
+    }
+    function archive(e) {
+        let movieName = e.currentTarget.parentElement.parentElement.querySelector('span').textContent
+        let priceOfTicket = e.currentTarget.parentElement.querySelector('strong').textContent
+        priceOfTicket = Number(priceOfTicket)
+        let ticketSold = e.currentTarget.parentElement.querySelector('input').value
+        ticketSold = Number(ticketSold);
+        let totalSum = priceOfTicket * ticketSold
+        if (!ticketSold) {
+            return
+        }
+        let liRootEl = document.createElement('li')
+        let spanNameEl = document.createElement('span')
+        spanNameEl.textContent = movieName
+        let strongTotalPriceEl = document.createElement('strong')
+        strongTotalPriceEl.textContent = `Total amount: ${totalSum.toFixed(2)}`
+        let deleteBtn = document.createElement('button')
+        deleteBtn.textContent = 'Delete'
+
+        deleteBtn.addEventListener('click', deleteCurrentLi)
+
+        liRootEl.appendChild(spanNameEl)
+        liRootEl.appendChild(strongTotalPriceEl)
+        liRootEl.appendChild(deleteBtn)
+        ulArchiveRoot.appendChild(liRootEl)
+        e.currentTarget.parentElement.parentElement.remove()
+    }
+
+    function deleteCurrentLi(e) {
+        e.currentTarget.parentElement.remove()
     }
 }
